6094657fcbc38c85060bc0a45306fcddd11e784f
zzezzee <GitHub zzezzee@naver.com>
2022-12-20 17:15:57 +0900

    프로젝트 세팅
    의존성 추가, CORS, ignore security

---
+++ src/main/java/com/zzezze/friendy/FriendyApplication.java
@@ -0,0 +1,32 @@
+package com.zzezze.friendy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class FriendyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendyApplication.class, args);
+	}
+
+	@Bean
+	public WebSecurityCustomizer ignoringCustomizer() {
+		return (web) -> web.ignoring().requestMatchers("/**");
+	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+}
