e31e1197b61b1fb81b1d20b5e85fef0622b6373f
zzezzee <GitHub zzezzee@naver.com>
2023-01-12 23:28:57 +0900

    일촌신청 기능

---
+++ src/test/java/com/zzezze/friendy/applications/CreateInvitationServiceTest.java
@@ -0,0 +1,45 @@
+package com.zzezze.friendy.applications;
+
+import com.zzezze.friendy.dtos.UserDto;
+import com.zzezze.friendy.models.Invitation;
+import com.zzezze.friendy.models.User;
+import com.zzezze.friendy.models.value_objects.Nickname;
+import com.zzezze.friendy.models.value_objects.Username;
+import com.zzezze.friendy.repositories.InvitationRepository;
+import com.zzezze.friendy.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class CreateInvitationServiceTest {
+    InvitationRepository invitationRepository;
+    UserRepository userRepository;
+    CreateInvitationService createInvitationService;
+
+    @BeforeEach
+    void setup() {
+        invitationRepository = mock(InvitationRepository.class);
+        userRepository = mock(UserRepository.class);
+        createInvitationService = new CreateInvitationService(invitationRepository, userRepository);
+    }
+
+    @Test
+    void create() {
+        Username username = new Username("zzezze");
+        Nickname nickname = new Nickname("jenna");
+
+        given(userRepository.findByNickname(nickname))
+                .willReturn(Optional.of(User.fake()));
+
+        UserDto userDto = createInvitationService.create(username, nickname);
+
+        verify(invitationRepository).save(any());
+    }
+}
